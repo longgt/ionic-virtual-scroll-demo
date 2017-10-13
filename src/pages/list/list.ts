50c5392d542778cf1b1b841de23485fcb8089b24
longgt <longgt@aureole-it.vn>
2017-10-13 11:06:39 +0700

    feat (init): add demo source

---
+++ src/pages/list/list.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ItemDetailPage } from '../item-detail/item-detail';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+    
+    this.items = [];
+    for (let i = 1; i < 100; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+}
